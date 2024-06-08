8e1a1aa3381d352f89f61bc63a6c05253fad4e21
Amit Mehta <amitmehta.aws@gmail.com>
2024-06-09 03:12:12 +0530

    upload data to dynamo db table working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import boto3
 
 s3_client = boto3.client('s3')
+db_client = boto3.client('dynamodb')
+table = 'S3_object_metadata'
 
 
 def get_metadata(bucket, key):
@@ -10,8 +12,17 @@ def get_metadata(bucket, key):
         Key=key
     )
     content_type = metadata['ContentType']
-
     return content_type
+
+
+def get_userid(bucket, key):
+
+    tagset = s3_client.get_object_tagging(
+        Bucket=bucket,
+        Key=key
+    )
+    user_id = tagset['TagSet'][0]['Value']
+    return user_id
 
 
 def lambda_handler(event, context):
@@ -23,13 +34,28 @@ def lambda_handler(event, context):
     region = event['Records'][0]['awsRegion']
 
     content_type = get_metadata(bucket, key)
+    user_id = get_userid(bucket, key)
+
+    db_client.put_item(
+        TableName=table,
+        Item={
+            'bucket': {'S': bucket},
+            'key': {'S': key},
+            'region': {'S': region},
+            'user_id': {'S': user_id},
+            'size': {'S': str(size)},
+            'content_type': {'S': content_type},
+            'timestamp': {'S': timestamp},
+        }
+    )
 
     return {
         'statusCode': 200,
-        'contentType': content_type,
-        'region': region,
-        'timestamp': timestamp,
-        'bucket': bucket,
-        'key': key,
-        'size': size,
     }
+
+# User-ID: admin
+# Bucket: image-store-bucket-891377355669
+# Region: us-east-1
+# Key: 04-06-2024/input.txt
+# Size: 10272
+# Timestamp: 2024-06-08T20:49:02.834Z
